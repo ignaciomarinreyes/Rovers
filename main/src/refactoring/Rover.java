4f87ddb88686d08262271959876e901b8dd88cee
ignaciomarinreyes <ignaciomarinreyes@gmail.com>
2020-10-11 15:24:31 +0100

    Inicial

---
+++ main/src/refactoring/Rover.java
@@ -0,0 +1,70 @@
+package refactoring;
+
+public class Rover {
+
+    public Rover(String facing, int x, int y) {
+    }
+
+    public Rover(Heading heading, int x, int y) {
+    }
+
+    public enum Heading {
+        North, East, South, West;
+
+        public static Heading of(String label) {
+            return of(label.charAt(0));
+        }
+
+        public static Heading of(char label) {
+            if (label == 'N') return North;
+            if (label == 'S') return South;
+            if (label == 'W') return West;
+            if (label == 'E') return East;
+            return null;
+        }
+
+        public Heading turnRight() {
+            return values()[add(+1)];
+        }
+
+        public Heading turnLeft() {
+            return values()[add(-1)];
+        }
+
+        private int add(int offset) {
+            return (this.ordinal() + offset + values().length) % values().length;
+        }
+
+    }
+
+    public static class Position {
+        private final int x;
+        private final int y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Position forward(Heading heading) {
+            return null;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            return isSameClass(object) && equals((Position) object);
+        }
+
+        private boolean equals(Position position) {
+            return position == this || (x == position.x && y == position.y);
+        }
+
+        private boolean isSameClass(Object object) {
+            return object != null && object.getClass() == Position.class;
+        }
+
+    }
+
+
+}
+
---
+++ main/test/com/codemanship/marsrover/Position_.java
@@ -0,0 +1,15 @@
+package com.codemanship.marsrover;
+
+import org.junit.Test;
+import refactoring.Rover.Position;
+
+import static org.junit.Assert.assertEquals;
+import static refactoring.Rover.Heading.*;
+
+public class Position_ {
+
+    @Test
+    public void should_calculate_forward_position() {
+        //assertEquals(new Position(-1, 0), new Position(0, 0).forward(North).forward(West).forward(South));
+    }
+}
